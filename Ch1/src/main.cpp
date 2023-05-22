6331ae89bbfe882265cd5acc29efca313bc00fb1
Stephen Nutman <stephen.nutman@ntc-europe.co.uk>
2023-05-22 09:00:31 +0100

    basic variables

--- Ch1/src/main.cpp
+++ Ch1/src/main.cpp
@@ -7,6 +7,18 @@ int main()
     int n = 42;
 
     std::cout << "Value of n is: " << n << std::endl;
+
+    bool a = false;
+    char b = 'b';
+    float c = 3.1416f;
+    unsigned int d = -82;
+
+    std::cout << "Value of a is: " << a << std::endl;
+    std::cout << "Value of b is: " << b << std::endl;
+    std::cout << "Value of c is: " << c << std::endl;
+    std::cout << "Value of d is: " << d << std::endl;
+
+
     
     return 0;
 }
